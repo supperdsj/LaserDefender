228f3ce50ba5c664e83953906995d62dacf6699f
dongsj <232171773@qq.com>
2019-04-08 19:23:31 +0800

    finish enemy move path

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int wayPointIndex = 0;
+
+
+    void Start() {
+        transform.position = waypoints[wayPointIndex].transform.position;
+    }
+
+    void Update() {
+        Move();
+    }
+
+    void Move() {
+        if (wayPointIndex < waypoints.Count) {
+            var targetPosition = waypoints[wayPointIndex].transform.position;
+            transform.position = Vector2.MoveTowards(transform.position, 
+                targetPosition,
+                moveSpeed * Time.deltaTime);
+            if (transform.position == targetPosition) {
+                wayPointIndex++;
+            }
+        }
+        else {
+            Destroy(gameObject);
+        }
+    }
+}
