bcac2208f43b60ca4fbad1412e7e5febf3ec87fe
dongsj <232171773@qq.com>
2019-04-09 16:03:21 +0800

    handle enemy damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    [SerializeField] float health = 100;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        DamageDealer damageDealer=other.gameObject.GetComponent<DamageDealer>();
+        damageDealer.Hit();
+        health -= damageDealer.GetDamage();
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
