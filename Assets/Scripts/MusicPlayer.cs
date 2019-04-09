f4bee5232c6683046928600d4f080a70f1a1442f
dongsj <232171773@qq.com>
2019-04-09 21:06:21 +0800

    add Singleton Music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    void Awake() {
+        SetUpSingleton();
+    }
+
+    void SetUpSingleton() {
+        if (FindObjectsOfType(GetType()).Length > 1) {
+            Destroy(gameObject);
+        }
+        else {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
